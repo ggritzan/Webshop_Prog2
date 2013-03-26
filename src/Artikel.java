46b3aaec0cf03afaeb6126f9e5e77f2cc3148d2c
Giacomo Gritzan <giacomo.gritzan@hotmail.de>
2013-03-26 16:56:07 +0100

    Test

---
+++ src/Artikel.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Giacomo
+ * Date: 26.03.13
+ * Time: 16:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class Artikel {
+}
