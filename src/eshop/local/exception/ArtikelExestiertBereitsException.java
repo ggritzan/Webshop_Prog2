62203784cb5a179e2c6def36fb376cc85426103c
Giacomo Gritzan <giacomo.gritzan@hotmail.de>
2013-04-10 14:25:44 +0200

    Erstellung Exceptione für bereits vorhandene Artiekl

---
+++ src/eshop/local/exception/ArtikelExestiertBereitsException.java
@@ -0,0 +1,11 @@
+package eshop.local.exception;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Giacomo
+ * Date: 10.04.13
+ * Time: 14:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArtikelExestiertBereitsException {
+}
