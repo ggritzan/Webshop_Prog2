2c07d336ca02160b01ebf3c1b8d116847a8e2bdc
Giacomo Gritzan <giacomo.gritzan@hotmail.de>
2013-04-10 14:35:45 +0200

    Erstellung der Artikelverwaltung

---
+++ src/eshop/local/domain/ArtikelVerwaltung.java
@@ -0,0 +1,11 @@
+package eshop.local.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Giacomo
+ * Date: 10.04.13
+ * Time: 14:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArtikelVerwaltung {
+}
