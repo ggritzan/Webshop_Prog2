386a3c3028e6da14daec234b6f9257365b3c4573
ndoege <ndoege@hs-bremen.de>
2013-04-14 10:49:47 +0200

    Die Funktionen bestandErhoehen und preisAendern der Mitarbeiterklasse funktionieren wieder, rufen jetzt die vorhandenen setter Der Artikelklasse auf, um die Aenderung auszuführen. Die alte neuerArtikel Funktion ist erstmal auskommentiert, da sie erstens nicht mehr funktioniert und zweitens vermutlich so nicht mehr gebraucht werden kann.

--- src/eshop/local/valueobjects/Mitarbeiter.java
+++ src/eshop/local/valueobjects/Mitarbeiter.java
@@ -20,18 +20,23 @@ public class Mitarbeiter extends Person {
 
     // Methoden der Klasse Mitarbeiter
 
+    //Erhoeht den Bestand des Artikels um den gewünschten Wert
     public void bestandErhoehen(Artikel a, int wert)   {
-        a.bestandErhoehen(wert);
+        a.setBestand(wert);
     }
 
+    //Aendert den Preis des Artikels auf den neuen Preis
     public void preisAendern(Artikel a, double wert) {
-        a.neuerPreis(wert);
+        a.setPreis(wert);
     }
 
+    /* Nicht mehr funktionierende/benötigte Funkion?)
     public Artikel neuerArtikel(String bezeichnung, int nummer, double preis, int bestand) {
         Artikel  x = new Artikel(bezeichnung,nummer,preis,bestand);
         return x;
     }
+    */
+
     // Setter
 
 
