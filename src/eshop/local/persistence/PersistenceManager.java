a297bd8e54f3f23f22f28e23659d37211ca5cace
ndoege <ndoege@hs-bremen.de>
2013-06-15 11:07:09 +0200

    Mehr Javadoc

--- src/eshop/local/persistence/PersistenceManager.java
+++ src/eshop/local/persistence/PersistenceManager.java
@@ -24,39 +24,67 @@ public interface PersistenceManager {
         /**
          * Methode zum Einlesen der Artikeldaten aus einer externen Datenquelle.
          *
-         * @return Artikel-Objekt, wenn Einlesen erfolgreich, false null
+         * @return Artikel, wenn Einlesen erfolgreich, ansonsten null
          */
 
         public Artikel ladeArtikel() throws IOException, ClassNotFoundException;
-
-
-        /**
-        public HashMap<Integer,Artikel> ladeArtikel() throws IOException, ClassNotFoundException;
-        */
-
 
         /**
          * Methode zum Schreiben der Artikeldaten in eine externe Datenquelle.
          *
          * @param a Artikel-Objekt, das gespeichert werden soll
-         * @return true, wenn Schreibvorgang erfolgreich, false sonst
+         * @return boolean, true, wenn Schreibvorgang erfolgreich, ansonsten false
          */
         public boolean speichereArtikel(Artikel a) throws IOException;
 
-        // public boolean speichereArtikel(HashMap<Integer,Artikel> artikelHashMap) throws IOException;
-
+        /**
+         * Methode zum Schreiben der MassengutArtikeldaten in eine externe Datenquelle.
+         *
+         * @param m MassenzgutArtikel-Objekt, das gespeichert werden soll
+         * @return boolean, true, wenn Schreibvorgang erfolgreich, ansonsten false
+         */
         public boolean speichereMassengutArtikel(MassengutArtikel m) throws IOException;
 
+        /**
+        * Methode zum auslesen der Kundendaten aus externer Datenquelle
+        *
+        * @return Kunde, wenn kein Kundenobjekt vorhanden ist null
+        */
         public Kunde ladeKunde() throws IOException, ClassNotFoundException;
 
+        /**
+        * Methode zum speichern eines Kundenobjekts in eine externe Datei
+        *
+        * @return boolean, true, wenn speichern erfolgreich, ansonsten false
+        */
         public boolean speichereKunde(Kunde k) throws IOException;
 
+        /**
+        * Methode zum ausleesen der Mitarbeiterdaten aus einer externen Datei
+        *
+        * @return Mitarbeiter-Objekt, ansonsten null
+        */
         public Mitarbeiter ladeMitarbeiter() throws IOException, ClassNotFoundException;
 
+        /**
+        * Methode zum speichern eines Mitarbeiterobjekts in eine externe Datei
+        *
+        * @return boolean, true, wenn speichern erfolgreich, ansonsten false
+        */
         public boolean speichereMitarbeiter(Mitarbeiter m) throws IOException;
 
+        /**
+        * Methode zum auslesen eines Rechnungs-Objekts aus einer externen Datei
+        *
+        * @return Rechnung, ansonsten null
+        */
         public Rechnung ladeRechnung() throws IOException, ClassNotFoundException;
 
+        /**
+        * Methode zum speichern eines Rechnungobjekts in eine externe Datei
+        *
+        * @return boolean, true, wenn speichern erfolgreich, ansonsten false
+        */
         public boolean speichereRechnung(Rechnung r) throws IOException;
 }
 
